d715c534165335bf20f9f60ca27e97c57a7eae3b
Peter Focko <fockojr@gmail.com>
2019-08-05 23:44:20 +0200

    Add core functionality

--- shellTemplates.sh
+++ shellTemplates.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo "Hello world!"
+envVarNames=$(env | grep -o '^[A-Za-z_][A-Za-z0-9_]*=' | tr -d '=')
+
+for envVarName in $envVarNames
+do
+    eval envVarValue=\$$envVarName
+    sed -i "s^<%\$ $envVarName \$%>^$envVarValue^g" "$1"
+done
