41fe6f44893c1523e7b64f6a33d5e6e992112209
Peter Focko <fockojr@gmail.com>
2019-08-07 21:15:01 +0200

    Refactor core functionality to use awk instead of sed

--- shellTemplates.sh
+++ shellTemplates.sh
@@ -4,6 +4,11 @@ envVarNames=$(env | grep -o '^[A-Za-z_][A-Za-z0-9_]*=' | tr -d '=')
 
 for envVarName in $envVarNames
 do
-    eval envVarValue=\$$envVarName
-    sed -i "s^<%\$ $envVarName \$%>^$envVarValue^g" "$1"
+    eval envVarValue=\$$envVarName    
+    awkExpr=$awkExpr"sub(\"<%\\\\$ $envVarName \\\\$%>\", \"$envVarValue\"); "
 done
+
+awkExpr='{'$awkExpr'print}'
+
+awk "$awkExpr" $1 > $1.tmp
+mv $1.tmp $1
