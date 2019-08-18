e0221de98d051ce416adf5897ad666a19b134d3d
Peter Focko <fockojr@gmail.com>
2019-08-18 17:50:09 +0200

    Add tests for multiline expressions with special characters

--- tests/env.sh
+++ tests/env.sh
@@ -5,3 +5,4 @@ export YEARS_OLD=22
 export HOME_CITY=Kosice
 export MY_FRIEND=Istvan
 export SECRET_MSG="Hope this test will work :)"
+export MULTILINE_EXAMPLE="Hello<%!SP!%>world!<%!NL!%><%!TB!%><%!DL!%>ample<%!SP!%>text<%!SP!%>with<%!SP!%><%!BS!%>"
