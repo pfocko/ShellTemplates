97da5cd741a9b43e5487b36b5df28ea7db97f67e
Peter Focko <fockojr@gmail.com>
2019-08-18 18:00:53 +0200

    Add comments to shellTemplates.sh

--- shellTemplates.sh
+++ shellTemplates.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
+# Load names of all environment variables
 envVarNames=$(env | grep -o '^[A-Za-z_][A-Za-z0-9_]*=' | tr -d '=')
 
-unset awkExpr
+unset replaceExpr
 
 for envVarName in $envVarNames
 do
+    # Get value of environment variable
     eval envVarValue=\$$envVarName
+
+    # Replace special characters in environment variable with shellTemplates constant expressions
     envVarValue=$(echo $envVarValue | awk '{gsub(" ","<%!SP!%>");gsub("\t","<%!TB!%>");gsub("$","<%!NL!%>");gsub("\\$","<%!DL!%>");gsub("\\\\","<%!BS!%>");print}')
-    envVarValue=$(echo $envVarValue | tr -d ' ' | awk '{gsub("<%!NL!%>$", "");print}')    
-    awkExpr=$awkExpr"gsub(\"<%\\\\$ $envVarName \\\\$%>\",\"$envVarValue\");"
+    envVarValue=$(echo $envVarValue | tr -d ' ' | awk '{gsub("<%!NL!%>$", "");print}')
+
+    # Append gsub function to awk replace expression
+    replaceExpr=$replaceExpr"gsub(\"<%\\\\$ $envVarName \\\\$%>\",\"$envVarValue\");"
 done
 
-awkExpr='{'$awkExpr'print}'
+# Assemble complete awk expression
+replaceExpr='{'$replaceExpr'print}'
 
 function interpretFile {
-    awk "$awkExpr" $1 > $1.tmp
+    # Interpret shellTemplates variable expressions
+    awk "$replaceExpr" $1 > $1.tmp
+
+    # Interpret shellTemplates constant expressions
     awk '{gsub("<%!SP!%>"," ");gsub("<%!TB!%>","\t");gsub("<%!DL!%>","$");gsub("<%!BS!%>","\\");gsub("<%!NL!%>","\n");print}' $1.tmp > $1
     rm $1.tmp
 }
